e6421084dc08a1bfab777b0b6f011cce88230181
DuBori <92709439+DuBori@users.noreply.github.com>
2022-03-30 19:38:18 +0900

    1주차 html css 적용

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 const toggleBtn = document.querySelector('.hbr__toogle');
-const menu = document.querySelector('.menu');
-const sns = document.querySelector('.sns__img');
+const menu_sns = document.querySelectorAll('ul');
 
 const swapbtn = document.querySelector('.nxt_btn');
 const swapimg = document.getElementById('main_i');
+
+const swapback = document.querySelector('.nxt_btn2');
+const swapcolor = document.querySelectorAll('.main_txt');
 
 var img_list =["./img/ante-samarzija-lsmu0rUhUOk-unsplash.jpg","./img/AKR20150416051900848_01_i_P4.jpg",
 "./img/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg","./img/jeremy-yap-jn-HaGWe4yw-unsplash.jpg"];
 
 var cnt = 1;
+var act_var = 'active';
+var my_event ={
+    // SetToggle funcion on/off
+    SetToggle :function(self,x){
+        self.addEventListener('click',()=>{
+            for (var i in x){
+                x[i].classList.toggle(act_var); 
+            }   
+    })}
+};
 
-toggleBtn.addEventListener('click',() =>{
-    menu.classList.toggle('active');
-    sns.classList.toggle('active');
-});
+my_event.SetToggle(toggleBtn,menu_sns);
+my_event.SetToggle(swapback,swapcolor);
 
-
+// main img list function
 swapbtn.addEventListener('click',() =>{
+    swapimg.src = img_list[cnt];
     if(cnt < img_list.length){
-        swapimg.src = img_list[cnt];
-        cnt= cnt + 1;
+        cnt+=1;
     }else{
-        cnt = 0;
-        alert('처음');
-        swapimg.src = img_list[cnt];
+        swapimg.src = img_list[0];
+        cnt =1;
     }
 });
+
 
